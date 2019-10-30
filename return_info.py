68e8c863920a8b43a5ab03fabfb56707060386ba
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 05:39:28 -0400

    Create return_info.py

---
+++ return_info.py
@@ -0,0 +1,9 @@
+#call a function and return info
+def times(x):
+    return x*x*x        #note return ends function.
+print(times(10))
+#more advanced
+def timess(x):          #def function
+    return x*x*x        # return info from function
+storing_info = timess(3)# storing info to be returned.  
+print(storing_info)     #call storage to print.
